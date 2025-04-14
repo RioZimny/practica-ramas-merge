1ff470ba5101a446a1753c095870cbbea9eda5a1
RioZimny <riozimny@gmail.com>
2025-04-14 13:33:24 -0300

    borrar des script.js

--- script.js
+++ script.js
@@ -12,9 +12,3 @@ function agregarALaPantalla(value) {
     document.getElementById('pantalla').value = '';
   }
 
-  function borrarUltimp(){
-
-    const valorPantalla=document.getElementById('pantalla').value;
-    document.getElementById('pantalla').value= valorPantalla.slice(0,-1)
-    
-  } 
