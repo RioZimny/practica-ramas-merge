d3ab05addb95dfcedce76f6dd5308184797d09e2
RioZimny <riozimny@gmail.com>
2025-04-14 11:59:56 -0300

    otra forma de deshacer

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ function agregarALaPantalla(value) {
     document.getElementById('pantalla').value = '';
   }
 
-  function deshacer(){
-    const valorPantalla=document.getElementById('pantalla').value;
-    document.getElementById('pantalla').value= valorPantalla.slice(0,-1)
-  } 
+function deshacer(){
+    const valorPantalla = document.getElementById('pantalla').value;
+    const resultado=valorPantalla.substring(0, valorPantalla.length -1);
+    document.getElementById('pantalla').value=resultado;
+  }
